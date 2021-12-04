243b817d1beabbb0e2e4abad80339c85eaa90721
medingee <43713964+medingee@users.noreply.github.com>
2021-12-04 15:35:13 -0800

    added comments to day 4

--- Day4/onion.cpp
+++ Day4/onion.cpp
@@ -13,53 +13,64 @@ int main(){
     const int SIZE = 5;
     const int SIZEOFLIST = SIZE*SIZE;
     ifstream myfile("input.txt");
-
+    //Checks if we have anything
     if(!myfile.is_open()){
         return -1;
     }
+    //bingo string
     vector<int> nums;
     string snumbers;
     myfile >> snumbers;
     stringstream ss(snumbers);
 
+    //convert bingo string into vector int
     for(int i; ss>>i;){
         nums.push_back(i);
         if (ss.peek() == ',') ss.ignore();
     }
 
+    //Create an array for to represent two tables:
+    //one for the actual ints, and one for the bools
     int table[SIZEOFLIST];
     bool btable [SIZEOFLIST];
 
     //mfw they have been asking for the sum of the unpicked numbers
-    //instead of the sum of winning bingo numbers.
+    //instead of the sum of winning bingo numbers. Too bad!
     int winningsum;
-    //PART 1
-    //int champ = nums.size();
     
+    //PART 1
+    int champ = nums.size();
     //PART 2
-    int champ = 0;
+    //int champ = 0;
+    
+    
     while(!myfile.eof()){
 
         for(int i = 0; i < SIZEOFLIST; i++){
             myfile >> table[i];
             btable[i] = false;
         }
+        bool found = false;
+
         //ENABLE FOR PART 1
-        /*
+        
         for( int i = 0; i < champ; i++){
             for(int j = 0; j < SIZEOFLIST; j++){
                 if(table[j] == nums.at(i)){
                     btable[j] = true;
                     int row = j/SIZE;
                     int col = j%SIZE;
+                    
+                    //Check for row bingo
                     bool hbingo = true;
-                    bool vbingo = true;
                     for(int k = 0; k < SIZE; k++){
                         if(!btable[k+row*SIZE]){
                             hbingo = false;
                             break;
                         }
                     }
+                    //check for column bingo
+                    bool vbingo = true;
                     for(int k = 0; k < SIZE; k++){
                         if(!btable[col+k*SIZE]){
                             vbingo = false;
@@ -69,19 +80,20 @@ int main(){
                     if(hbingo || vbingo){
                         //flush old value
                         winningsum = 0;
-                        for(int k = 0; k < SIZEOFLIST; k++){
-                             if(!btable[k]) winningsum += table[k];
-                        }
+                        for(int k = 0; k < SIZEOFLIST; k++) if(!btable[k]) winningsum += table[k];
                         champ = i;
+                        found = true;
                     }
                     break;
                 }
             }
+            if(found) break;
         }
-        */
         
+        
+        //basically the same comments
         //ENABLE FOR PART 2
-        bool found = false;
+        /*
         for( int i = 0; i < nums.size(); i++){
             for(int j = 0; j < SIZEOFLIST; j++){
                 if(table[j] == nums.at(i)){
@@ -118,18 +130,9 @@ int main(){
                 }
             }
             if(found) break;
-        }
+        }*/
     }
     
-    cout << champ << endl;
     cout << winningsum * nums.at(champ)<< endl;
-
-   
-    
-    //cout << "champ index: " << champ << endl;
-    
-
-    
-
     return 0;
 }
